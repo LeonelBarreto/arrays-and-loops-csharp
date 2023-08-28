8f49b1d94b12e3ecd78ef39673f7cf2aab6aa6a1
Leonel Barreto <leonelbarretoa@yahoo.com.br>
2023-08-28 17:59:16 -0300

    resolvido: exercicio 3

---
+++ ArraysAndLoops/Exercicio03/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Exercicio03;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arrayNumeros = { 54, 22, 14, 10, 87, 284};
+
+        Console.Write("Digite o número a ser procurado no array: ");
+        int numero = int.Parse(Console.ReadLine());
+
+
+        int buscarNumero = 0;
+
+        for(int i = 0; i < arrayNumeros.Length; i++)
+        {
+            if (numero == arrayNumeros[i])
+            {
+                buscarNumero = numero;
+            }
+        }
+
+        if (buscarNumero == 0)
+        {
+            Console.Write(-1);
+        }
+        else
+        {
+            Console.Write(buscarNumero);
+        }
+    }
+}
+
