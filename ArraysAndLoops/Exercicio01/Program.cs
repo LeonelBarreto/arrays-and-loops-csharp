4900c1b467a043afa8922e3748c031afcbf76095
Leonel Barreto <leonelbarretoa@yahoo.com.br>
2023-08-28 17:10:32 -0300

    resolvido: exercicio 1

---
+++ ArraysAndLoops/Exercicio01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercicio01;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Quantos números inteiros serão somados: ");
+        int tamanho = int.Parse(Console.ReadLine());
+
+        int[] numeros = new int[tamanho];
+
+        for(int i = 0; i < tamanho; i++)
+        {
+            numeros[i] = int.Parse(Console.ReadLine());
+        }
+
+        int soma = 0;
+
+        for(int i = 0; i < tamanho; i++)
+        {
+            soma += numeros[i];
+        }
+
+        Console.WriteLine("Soma total: ");
+        Console.WriteLine(soma);
+    }
+}
+
