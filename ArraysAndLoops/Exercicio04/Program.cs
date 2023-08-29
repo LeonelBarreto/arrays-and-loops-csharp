1f936226667385532cef80dd1f4c1a8ae67a1521
Leonel Barreto <leonelbarretoa@yahoo.com.br>
2023-08-29 10:19:15 -0300

    resolvido: exercicio 4

---
+++ ArraysAndLoops/Exercicio04/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exercicio04;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Escolha um número inteiro para saber seus múltiplos: ");
+        int multiplo = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i <= 500; i++)
+        {
+            int resultado = multiplo * i;
+            Console.WriteLine(resultado);
+        }
+    }
+}
+
