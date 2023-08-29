0f9b4cec62a599843542fdec3b8277e31375b756
Leonel Barreto <leonelbarretoa@yahoo.com.br>
2023-08-29 13:53:41 -0300

    resolvido: exercicio 6

---
+++ ArraysAndLoops/Exercicio06/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Exercicio06;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Insira a quantidade de números que irão compor o array: ");
+        int quantidade = int.Parse(Console.ReadLine());
+
+        int[] numeros = new int[quantidade];
+
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            numeros[i] = int.Parse(Console.ReadLine());
+        }
+
+        int somaPares = 0;
+        int somaImpares = 0;
+
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            if (numeros[i] % 2 == 0)
+            {
+                somaPares += numeros[i];
+            }
+            else
+            {
+                somaImpares += numeros[i];
+            }
+        }
+
+        Console.WriteLine();
+        Console.Write("Você deseja saber a doma dos números pares ou ímpares? Responda 'par'ou 'impar': ");
+        string resposta = Console.ReadLine().ToLower();
+
+        Console.WriteLine();
+        if (resposta == "par")
+        {
+            Console.Write(somaPares);
+        }
+        else if (resposta == "impar")
+        {
+            Console.Write(somaImpares);
+        }
+        else
+        {
+            Console.Write("Por favor inserir apenas 'par' ou 'impar'.");
+        }
+    }
+}
+
