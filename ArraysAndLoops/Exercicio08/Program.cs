f125340c877197bf0bcb6afe7c9dd445344dac8c
Leonel Barreto <leonelbarretoa@yahoo.com.br>
2023-08-29 15:38:24 -0300

    resolvido: exercicio 8

---
+++ ArraysAndLoops/Exercicio08/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Exercicio08;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite a quantidade de números inteiros que terá o array criado: ");
+        int quantidade = int.Parse(Console.ReadLine());
+
+        int[] numeros = new int[quantidade];
+
+        for (int i = 0; i < quantidade; i++)
+        {
+            numeros[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine();
+
+        int maiorNumero = 0;
+
+        foreach (var numero in numeros)
+        {
+            if ( numero > maiorNumero)
+            {
+                maiorNumero = numero;
+            }
+        }
+
+        Console.WriteLine($"O maior número encontrado foi: {maiorNumero}.");
+    }
+}
+
