5fb729f6451e7aac79baf11717f3bcc3aff6ca37
Leonel Barreto <leonelbarretoa@yahoo.com.br>
2023-08-29 14:48:34 -0300

    resolvido: exercicio 7

---
+++ ArraysAndLoops/Exercicio07/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Exercicio07;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Informe quantos nomes irão compor o array: ");
+        int quantidade = int.Parse(Console.ReadLine());
+
+        string[] nomes = new string[quantidade];
+
+        for (int i = 0; i < quantidade; i++)
+        {
+            nomes[i] = Console.ReadLine();
+        }
+
+        Console.WriteLine();
+
+        Console.Write("Informe a letra para criar o filtro com as palavras que iniciam com ela: ");
+        string letra = Console.ReadLine();
+
+        Console.WriteLine();
+
+        List<string> listaNomesFiltrados = new List<string>();
+
+        foreach (var nome in nomes.Where(nome => nome.StartsWith(letra)))
+        {
+            listaNomesFiltrados.Add(nome);
+        }
+
+        Console.Write(string.Join(", ", listaNomesFiltrados));
+    }
+}
+
