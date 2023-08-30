062aa72ee531179634855945961eb9eead890f64
Leonel Barreto <leonelbarretoa@yahoo.com.br>
2023-08-30 11:01:51 -0300

    resolvido: exercicio 10

---
+++ ArraysAndLoops/Exercicio10/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Exercicio10;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite a quantidade de números inteiros que serão adicionados ao array: ");
+        int quantidade = int.Parse(Console.ReadLine());
+
+        int[] numeros = new int[quantidade];
+
+        for (int i = 0; i < quantidade; i++)
+        {
+            numeros[i] = int.Parse(Console.ReadLine());
+        }
+
+        int maiorDiferenca = 0;
+
+        foreach (int x in numeros)
+        {
+            foreach (int y in numeros)
+            {
+                int diferencaAtual = x - y;
+
+                if (diferencaAtual > maiorDiferenca)
+                {
+                    maiorDiferenca = diferencaAtual;
+                }
+            }
+        }
+
+        Console.Write(maiorDiferenca);
+    }
+}
+
