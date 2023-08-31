afff5f8a88ee708133f1f6ba029b5161a81f6c77
Leonel Barreto <leonelbarretoa@yahoo.com.br>
2023-08-31 14:55:11 -0300

    resolvido: exercicio 2

--- ArraysAndLoops/Exercicio02/Program.cs
+++ ArraysAndLoops/Exercicio02/Program.cs
@@ -4,16 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        string[] array = { "A", "a", "E", "e", "E","i", "i", "O", "o", "U", "u" };
+        Console.Write("Informe a quantidade de letras que serão digitadas: ");
+        int quantidade = int.Parse(Console.ReadLine());
+
+        char[] letras = new char[quantidade];
+
+        for (int i = 0; i < quantidade; i++)
+        {
+            Console.Write($"{i + 1}/{quantidade}: ");
+            letras[i] = char.Parse(Console.ReadLine().ToLower());
+        }
 
         Console.Write("Digite a vogal a ser encontrada: ");
-        string vogal = Console.ReadLine();
+        char letra = char.Parse(Console.ReadLine().ToLower());
 
         int contador = 0;
 
-        for (int i = 0; i < array.Length; i++)
+        for (int i = 0; i < letras.Length; i++)
         {
-            if (vogal == array[i])
+            if (letra == letras[i])
             {
                 contador++;
             }
@@ -21,15 +30,15 @@ class Program
 
         if (contador == 0)
         {
-            Console.Write($"A letra {vogal} não foi encontrada.");
+            Console.Write($"A letra {letra} não foi encontrada.");
         }
         else if (contador == 1)
         {
-            Console.Write($"A letra {vogal} foi encontrada 1 vez.");
+            Console.Write($"A letra {letra} foi encontrada 1 vez.");
         }
         else
         {
-            Console.Write($"A letra {vogal} foi encontrada {contador} vezes.");
+            Console.Write($"A letra {letra} foi encontrada {contador} vezes.");
         }
     }
 }
