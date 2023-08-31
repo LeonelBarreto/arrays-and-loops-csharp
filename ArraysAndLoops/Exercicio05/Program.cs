2376bd7141c44d14d04dfba846bf8e44b25b4408
Leonel Barreto <leonelbarretoa@yahoo.com.br>
2023-08-31 14:59:19 -0300

    resolvido: exercicio 5

--- ArraysAndLoops/Exercicio05/Program.cs
+++ ArraysAndLoops/Exercicio05/Program.cs
@@ -9,14 +9,15 @@ class Program
     {
         Console.WriteLine("Insira a quantidade de números inteiros que serão inseridos no array:");
         int quantidade = int.Parse(Console.ReadLine());
+
         int[] numeros = new int[quantidade];
 
-        for (int i = 0; i < 7; i++)
+        for (int i = 0; i < quantidade; i++)
         {
+            Console.Write($"{i + 1}/{quantidade}: ");
             numeros[i] = int.Parse(Console.ReadLine());
         }
 
-        
         Console.WriteLine();
         Console.Write("Você deseja filtar os números ímpares ou pares? Escreva impar ou par: ");
         string filtro = Console.ReadLine().ToLower();
